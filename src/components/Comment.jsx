1ba47270e749c32924ab2ec39c8fcbd977b6e933
AndreAugustoJunior <aajraugusto@gmail.com>
2024-08-20 11:58:40 -0300

    dbjason

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -17,13 +17,13 @@ export function Comment({ content, onDeleteComment }) {
 
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/a.png" alt="" />
+      <Avatar hasBorder={false} src="https://github.com/AndreAugustoJunior.png" alt="" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
-              <strong>John Doe</strong>
+              <strong>Andre Junior</strong>
               <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">
                 Cerca de 1h atrás
               </time>
